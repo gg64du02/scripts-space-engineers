af13bbe17805407bddf0b98ddef5630586896e72
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-02-11 19:46:12 +0100

    added accelaration to it

--- display_infos_pos_speed_acc.cs
+++ display_infos_pos_speed_acc.cs
@@ -25,13 +25,19 @@ namespace SpaceEngineers
 //////////////////////////BEGIN//////////////////////////////////////////
 //=======================================================================
 
+double prev_v_x = 0;
+double prev_v_y = 0;
+
+double a_x = 0;
+double a_y = 0;
+
 public Program()
 {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script.
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;
-            //This makes the program automatically run every 10 ticks.
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script.
+    Runtime.UpdateFrequency = UpdateFrequency.Update1;
+    //This makes the program automatically run every 10 ticks.
 }
 public void Main()
 {
@@ -44,19 +50,36 @@ public void Main()
     Echo("myCurrentCockpit.RotationIndicator:" + myCurrentCockpit.RotationIndicator);
     var currentVelocity = myCurrentCockpit.GetShipVelocities();
     Echo("currentVelocity.LinearVelocity:" + currentVelocity.LinearVelocity);
-    Echo("myCurrentCockpit.Orientation:" + myCurrentCockpit.Orientation);
+    //useless
+    //Echo("myCurrentCockpit.Orientation:" + myCurrentCockpit.Orientation);
+
+    //how much time since the last loop
+    var dts = Runtime.TimeSinceLastRun.TotalSeconds;
+
+    //figuring out the acc for hor axis
+    a_x = (prev_v_x - currentVelocity.LinearVelocity.X) / dts;
+    a_y = (prev_v_y - currentVelocity.LinearVelocity.Y) / dts;
+
+    //storing current speeds for next loop
+    prev_v_x = currentVelocity.LinearVelocity.X;
+    prev_v_y = currentVelocity.LinearVelocity.Y;
+
 
     debugString += "\n" + "myCurrentCockpit.RotationIndicator:\n" +  myCurrentCockpit.RotationIndicator;
     debugString += "\n" + "currentVelocity.LinearVelocity:\n" + currentVelocity.LinearVelocity;
-    debugString += "\n" + "myCurrentCockpit.Orientation:\n" + myCurrentCockpit.Orientation;
-
+    debugString += "\n" + "a_x,a_y:\n" + a_x + "," + a_y;
+    //useless
+    //debugString += "\n" + "myCurrentCockpit.Orientation:\n" + myCurrentCockpit.Orientation;
 
 
     //lcd display
     var textPanel = GridTerminalSystem.GetBlockWithName("textPanel") as IMyTextPanel;
-    textPanel.WritePublicText("debugString", false);
-    textPanel.WritePublicTitle("debugString", false);
     textPanel.WriteText(debugString, false);
+    //deprecated
+    //textPanel.WritePublicText("debugString", false);
+    //textPanel.WritePublicTitle("debugString", false);
+
+
 }
 
         public void Save()
