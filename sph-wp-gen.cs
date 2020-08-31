94a93ea6f31ce50518d443c13e26297d5b5413d8
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-08-31 23:50:14 +0200

    Script body

--- sph-wp-gen.cs
+++ sph-wp-gen.cs
@@ -1,3 +1,40 @@
 //script would be based on
 //https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
 
+
+
+public Program()
+{
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed.
+    // 
+    // It's recommended to set RuntimeInfo.UpdateFrequency 
+    // here, which will allow your script to run itself without a 
+    // timer block.
+    Runtime.UpdateFrequency = UpdateFrequency.Update100;
+}
+
+public void Save()
+{
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed.
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    // The main entry point of the script, invoked every time
+    // one of the programmable block's Run actions are invoked,
+    // or the script updates itself. The updateSource argument
+    // describes where the update came from.
+    // 
+    // The method itself is required, but the arguments above
+    // can be removed if not needed.
+}
