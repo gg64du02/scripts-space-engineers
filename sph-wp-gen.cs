4c5156a51b386940f5d1a6d8edbb350c9134bb39
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-09-01 00:29:13 +0200

    First draft

--- sph-wp-gen.cs
+++ sph-wp-gen.cs
@@ -1,6 +1,27 @@
 //script would be based on
 //https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
 
+//sphere radius in meters
+double r = 60000;
+
+//Numbers of wanted points
+double N = 5000;
+
+//numbers of generated points
+double N_count = 0;
+
+double a = 0;
+double d = 0;
+
+
+double M_v = 0;
+double d_v = 0;
+double d_phi = 0;
+
+
+
+
+
 
 
 public Program()
@@ -16,6 +37,14 @@ public Program()
     // here, which will allow your script to run itself without a 
     // timer block.
     Runtime.UpdateFrequency = UpdateFrequency.Update100;
+    a = 4 * Math.PI * (r * r) / N;
+    d = Math.Sqrt(a);
+
+
+    M_v = Math.Round((Math.PI / d),3);
+    d_v = Math.PI / M_v;
+    d_phi = a / d_v;
+
 }
 
 public void Save()
@@ -37,4 +66,21 @@ public void Main(string argument, UpdateType updateSource)
     // 
     // The method itself is required, but the arguments above
     // can be removed if not needed.
+    Echo("a:" + a);
+    Echo("d:" + d);
+    Echo("M_v:" + M_v);
+    Echo("d_v:" + d_v);
+    Echo("d_phi:" + d_phi);
+
+    for (int m = 0; m < M_v; m++)
+    {
+        double v = Math.PI * (m + .5) / M_v;
+        double M_phi = Math.Round((2 * Math.PI * Math.sin(v) / d_v), 3);
+        for (int n = 0; n < M_phi; n++)
+        {
+            double phi = 2 * Math.PI * n / M_phi;
+            //Create point using Eqn. (1).
+            N_count = N_count + 1;
+        }
+    }
 }
