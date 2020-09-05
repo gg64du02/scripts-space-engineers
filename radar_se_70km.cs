3cab424cf064a35e55d5a3bd98290ec97a6ecc03
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-09-05 15:30:46 +0200

    First testing of the radar

--- radar_se_70km.cs
+++ radar_se_70km.cs
@@ -1,3 +1,64 @@
+double range_to_test_at = 300;
+
+
+//Constants to generate equidistributed points on the surface of a sphere
+//sphere radius in meters
+double r = 1;
+
+//Numbers of wanted points
+double N = 500;
+
+//numbers of generated points
+double N_count = 0;
+
+double a = 0;
+double d = 0;
+
+double M_v = 0;
+double d_v = 0;
+double d_phi = 0;
+
+double planetRadius = 60000;
+
+List<Vector3D> pointsToScan = null;
+
+
+public List<Vector3D> generateWaypoints(IMyRemoteControl remote)
+{
+    List<Vector3D> generatedPoints = new List<Vector3D>();
+
+    for (int m = 0; m < M_v; m++)
+    {
+        double v = Math.PI * (m + .5) / M_v;
+        double M_phi = Math.Round((2 * Math.PI * Math.Sin(v) / d_v), 3);
+        for (int n = 0; n < M_phi; n++)
+        {
+            double phi = 2 * Math.PI * n / M_phi;
+            //Create point using Eqn. (1).
+            double x = planetRadius * r * Math.Sin(v) * Math.Cos(phi);
+            double y = planetRadius * r * Math.Sin(v) * Math.Sin(phi);
+            double z = planetRadius * r * Math.Cos(phi);
+            Vector3D generatedPoint = new Vector3D(x, y, z);
+
+            Vector3D tmpCenterOfPlanet = new Vector3D(0, 0, 0);
+            if (remote != null)
+            {
+                remote.TryGetPlanetPosition(out tmpCenterOfPlanet);
+                generatedPoint += tmpCenterOfPlanet;
+            }
+            if(remote == null)
+            {
+                generatedPoint += Me.GetPosition();
+            }
+            //Echo("generatedPoint:"+ generatedPoint);
+            generatedPoints.Add(generatedPoint);
+            N_count = N_count + 1;
+        }
+    }
+
+    return generatedPoints;
+}
+
 public Program()
 {
     // The constructor, called only once every session and
@@ -11,6 +72,17 @@ public Program()
     // here, which will allow your script to run itself without a 
     // timer block.
     Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
+
+    a = 4 * Math.PI * (r * r) / N;
+    d = Math.Sqrt(a);
+
+
+    M_v = Math.Round((Math.PI / d), 3);
+    d_v = Math.PI / M_v;
+    d_phi = a / d_v;
+
+
 }
 
 public void Save()
@@ -44,18 +116,59 @@ public void Main(string argument, UpdateType updateSource)
         cameraBlock.EnableRaycast = true;
     }
 
+    if (pointsToScan == null)
+    {
+        pointsToScan = generateWaypoints(null);
+    }
+
+    //Echo("N:" + N);
+    //Echo("a:" + a);
+    //Echo("d:" + d);
+    //Echo("M_v:" + M_v);
+    //Echo("d_v:" + d_v);
+    //Echo("d_phi:" + d_phi);
+
     //degree not radians ?
     Echo("RaycastConeLimit " + cameraBlock.RaycastConeLimit);
 
     Echo("Position " + cameraBlock.Position);
 
-    Echo("" + Me.Position);
+    //Echo("" + Me.Position);
 
-    Echo("" + Me.GetPosition());
+    //Echo("" + Me.GetPosition());
 
     Echo("RaycastDistanceLimit " + cameraBlock.RaycastDistanceLimit);
 
     Echo("AvailableScanRange " + cameraBlock.AvailableScanRange);
+
+    Echo("pointsToScan.Count "+ pointsToScan.Count);
+	
+	if(cameraBlock.AvailableScanRange> range_to_test_at)
+    {
+        Echo("AvailableScanRange> range_to_test_at");
+        bool canScanOnePoint = false;
+        Vector3D pointAboutToBeScanned = new Vector3D(0,0,0);
+        foreach(Vector3D point in pointsToScan)
+        {
+            //Echo("point " + point);
+            //CanScan(Vector3D)   Checks if the camera can scan to the given target
+            if (cameraBlock.CanScan(point) == true)
+            {
+                canScanOnePoint = true;
+                pointAboutToBeScanned = point;
+                break;
+            }
+        }
+        if(canScanOnePoint == true)
+        {
+            //Echo("point " + point);
+            var result = cameraBlock.Raycast(pointAboutToBeScanned);
+            Echo("result " + result);
+            Echo("result.HitPosition " + result.HitPosition);
+            pointsToScan.Remove(pointAboutToBeScanned);
+        }
+        
+    }
 }
 
 
