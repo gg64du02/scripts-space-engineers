bf7181563d09a98cbc05870e8e313a6e9b5870cf
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-11-13 00:25:24 +0100

    Adding constants to interface the controller with the PB

--- handle-style-rover.cs
+++ handle-style-rover.cs
@@ -33,6 +33,16 @@ public void Main(string argument, UpdateType updateSource)
     // The method itself is required, but the arguments above
     // can be removed if not needed.
 	
+	
+    List<IMyRadioAntenna> listAntenna = new List<IMyRadioAntenna>();
+    GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(listAntenna);
+	
+	bool tryToStandStill = false;
+	bool tryToGoFoward = false;
+	bool tryToGoBackward = false;
+	bool tryToGoRight = false;
+	bool tryToGoLeft = false;
+	
 	Echo("lol1");
 	
 	IMyShipController shipController = null;
@@ -40,6 +50,47 @@ public void Main(string argument, UpdateType updateSource)
 
 	List<IMyShipController> shipControllers = new List<IMyShipController>();
 	GridTerminalSystem.GetBlocksOfType<IMyShipController>(shipControllers);
+	
+	if(shipControllers.Count!=0){
+		shipController = shipControllers[0];
+		if(shipController.IsUnderControl == true){
+			Echo("isManned");
+			//TODO
+			Vector3D moveIndicator = shipController.MoveIndicator;
+			Echo("moveIndicator:"+moveIndicator);
+			
+			if(moveIndicator.Z!=0){
+				if(moveIndicator.Z ==1){
+					tryToGoBackward = true;
+				}
+				else{
+					tryToGoFoward = true;
+				}
+			}
+			if(moveIndicator.X !=0){
+				if(moveIndicator.X ==1){
+					tryToGoRight = true;
+				}
+				else{
+					tryToGoLeft = true;
+				}
+			}
+			
+		}
+		else{
+			Echo("isNotManned");
+			tryToStandStill = true;
+		}
+	}
+	
+	var str_to_display = ""+moveIndicator;
+	if (listAntenna.Count != 0)
+	{
+		//var str_to_display = "133";
+		listAntenna[0].HudText = str_to_display;
+	}
+	shipController.CubeGrid.CustomName = str_to_display;
+	
 			
 	
 	Echo("lol2");
