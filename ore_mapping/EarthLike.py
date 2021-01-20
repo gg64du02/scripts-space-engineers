3dd1f62dfefb00ed244178f191d909e488b614c4
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2021-01-20 22:30:34 +0100

    Adding the XML parsing and ore/value links

--- ore_mapping/EarthLike.py
+++ ore_mapping/EarthLike.py
@@ -57,6 +57,114 @@ display_surface_lacks_bool = False
 display_ore_bool = True
 
 from skimage import measure
+
+# =============================================
+# =============================================
+# =============================================
+
+import xml.etree.ElementTree as ET
+
+mytree = ET.parse("PlanetGeneratorDefinitions.sbc")
+myroot = mytree.getroot()
+# print(myroot)
+
+i = 0
+
+FeValuesList = []
+NiValuesList = []
+SiValuesList = []
+CoValuesList = []
+AgValuesList = []
+MgValuesList = []
+UrValuesList = []
+AuValuesList = []
+
+for x in myroot[0]:
+    # print(x.tag, x.attrib)
+    if(x.tag == "OreMappings"):
+        pass
+        for y in x:
+            i += 1
+            # print("i:",i)
+            # print(y.tag, y.attrib)
+            # print(y.attrib["Value"])
+            oreValue = int(y.attrib["Value"])
+            oreTypeStr = y.attrib["Type"]
+
+
+            oreAbrStr = ""
+            if("Iron" in oreTypeStr):
+                oreAbrStr = "Fe"
+                # print("yyy")
+                if(oreValue not in FeValuesList):
+                    FeValuesList.append(oreValue)
+                # print("FeValuesList:",FeValuesList)
+            if("Nickel" in oreTypeStr):
+                oreAbrStr = "Ni"
+                if(oreValue not in FeValuesList):
+                    NiValuesList.append(oreValue)
+            if("Silicon" in oreTypeStr):
+                oreAbrStr = "Si"
+                if(oreValue not in FeValuesList):
+                    SiValuesList.append(oreValue)
+            if("Cobalt" in oreTypeStr):
+                oreAbrStr = "Co"
+                if(oreValue not in FeValuesList):
+                    CoValuesList.append(oreValue)
+            if("Silver" in oreTypeStr):
+                oreAbrStr = "Ag"
+                if(oreValue not in FeValuesList):
+                    AgValuesList.append(oreValue)
+            if("Magnesium" in oreTypeStr):
+                oreAbrStr = "Mg"
+                if(oreValue not in FeValuesList):
+                    MgValuesList.append(oreValue)
+            if("Uraninite" in oreTypeStr):
+                oreAbrStr = "Ur"
+                if(oreValue not in FeValuesList):
+                    UrValuesList.append(oreValue)
+            if("Gold" in oreTypeStr):
+                oreAbrStr = "Au"
+                if(oreValue not in FeValuesList):
+                    AuValuesList.append(oreValue)
+            # print("oreAbrStr:",oreAbrStr)
+            # else:
+            #     pass
+            #     # print("nnn")
+        break
+
+
+def whatOreThatValueIs(valueInt):
+    global FeValuesList
+    global NiValuesList
+    global SiValuesList
+    global CoValuesList
+    global AgValuesList
+    global MgValuesList
+    global UrValuesList
+    global AuValuesList
+    if(valueInt in FeValuesList):
+        return "Fe"
+    if(valueInt in NiValuesList):
+        return "Ni"
+    if(valueInt in SiValuesList):
+        return "Si"
+    if(valueInt in CoValuesList):
+        return "Co"
+    if(valueInt in AgValuesList):
+        return "Ag"
+    if(valueInt in MgValuesList):
+        return "Mg"
+    if(valueInt in UrValuesList):
+        return "Ur"
+    if(valueInt in AuValuesList):
+        return "Au"
+    return ""
+
+# =============================================
+# =============================================
+# =============================================
+
 for i in range(6):
     # print(i)
     folder_planetsfiles = 'planets_files/EarthLike/'
