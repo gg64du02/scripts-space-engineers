2ee0be0652745e0b57e2c67fdcd2f48b86701e40
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2021-01-22 20:38:14 +0100

    Updating the parsing script

--- ore_mapping/parsingXml.py
+++ ore_mapping/parsingXml.py
@@ -1,15 +1,18 @@
 
 linkToXmlOreRules = "https://raw.githubusercontent.com/KeenSoftwareHouse/SpaceEngineers/master/Sources/SpaceEngineers/Content/Data/PlanetGeneratorDefinitions.sbc"
-
-# print(linkToXmlOreRules)
+# =============================================
+# =============================================
+# =============================================
 
 import xml.etree.ElementTree as ET
 
+# file from https://github.com/KeenSoftwareHouse/SpaceEngineers/blob/master/Sources/SpaceEngineers/Content/Data/PlanetGeneratorDefinitions.sbc
 mytree = ET.parse("PlanetGeneratorDefinitions.sbc")
 myroot = mytree.getroot()
 # print(myroot)
 
 i = 0
+previousGPScoords = [0,0,0]
 
 FeValuesList = []
 NiValuesList = []
@@ -42,37 +45,69 @@ for x in myroot[0]:
                 # print("FeValuesList:",FeValuesList)
             if("Nickel" in oreTypeStr):
                 oreAbrStr = "Ni"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in NiValuesList):
                     NiValuesList.append(oreValue)
             if("Silicon" in oreTypeStr):
                 oreAbrStr = "Si"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in SiValuesList):
                     SiValuesList.append(oreValue)
             if("Cobalt" in oreTypeStr):
                 oreAbrStr = "Co"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in CoValuesList):
                     CoValuesList.append(oreValue)
             if("Silver" in oreTypeStr):
                 oreAbrStr = "Ag"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in AgValuesList):
                     AgValuesList.append(oreValue)
             if("Magnesium" in oreTypeStr):
                 oreAbrStr = "Mg"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in MgValuesList):
                     MgValuesList.append(oreValue)
             if("Uraninite" in oreTypeStr):
                 oreAbrStr = "Ur"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in UrValuesList):
                     UrValuesList.append(oreValue)
             if("Gold" in oreTypeStr):
                 oreAbrStr = "Au"
-                if(oreValue not in FeValuesList):
+                if(oreValue not in AuValuesList):
                     AuValuesList.append(oreValue)
             # print("oreAbrStr:",oreAbrStr)
             # else:
             #     pass
             #     # print("nnn")
         break
+
+
+def whatOreThatValueIs(valueInt):
+    global FeValuesList
+    global NiValuesList
+    global SiValuesList
+    global CoValuesList
+    global AgValuesList
+    global MgValuesList
+    global UrValuesList
+    global AuValuesList
+    if(valueInt in FeValuesList):
+        return "Fe"
+    if(valueInt in NiValuesList):
+        return "Ni"
+    if(valueInt in SiValuesList):
+        return "Si"
+    if(valueInt in CoValuesList):
+        return "Co"
+    if(valueInt in AgValuesList):
+        return "Ag"
+    if(valueInt in MgValuesList):
+        return "Mg"
+    if(valueInt in UrValuesList):
+        return "Ur"
+    if(valueInt in AuValuesList):
+        return "Au"
+    return "$"
+print("checking xml lists")
+# =============================================
+# =============================================
+# =============================================
 print("FeValuesList:",FeValuesList)
 print("NiValuesList:",NiValuesList)
 print("SiValuesList:",SiValuesList)
