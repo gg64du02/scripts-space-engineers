a5aac7d2850aa7cfa351a598a2c2966bc69f2c4e
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2021-02-15 23:14:32 +0100

    Adding xml reading and generating the needed lines for the c# script

--- ore_mapping/OMSS intagrated into PB/preping patterns list/preping list pattern for cs_128_128.py
+++ ore_mapping/OMSS intagrated into PB/preping patterns list/preping list pattern for cs_128_128.py
@@ -33,6 +33,109 @@ data_ore_layer = 255 * data[:, :, 2]
 data_lack_layer = data_lack_layer.astype('int16')
 data_ore_layer = data_ore_layer.astype('int16')
 
+
+
+import xml.etree.ElementTree as ET
+
+# file from https://github.com/KeenSoftwareHouse/SpaceEngineers/blob/master/Sources/SpaceEngineers/Content/Data/PlanetGeneratorDefinitions.sbc
+mytree = ET.parse("PlanetGeneratorDefinitions.sbc")
+myroot = mytree.getroot()
+# print(myroot)
+
+FeValuesList = []
+NiValuesList = []
+SiValuesList = []
+CoValuesList = []
+AgValuesList = []
+MgValuesList = []
+UrValuesList = []
+AuValuesList = []
+
+for x in myroot[0]:
+    # print(x.tag, x.attrib)
+    if(x.tag == "OreMappings"):
+        pass
+        for y in x:
+            # i += 1
+            # print("i:",i)
+            # print(y.tag, y.attrib)
+            # print(y.attrib["Value"])
+            oreValue = int(y.attrib["Value"])
+            oreTypeStr = y.attrib["Type"]
+
+
+            oreAbrStr = ""
+            if("Iron" in oreTypeStr):
+                oreAbrStr = "Fe"
+                # print("yyy")
+                if(oreValue not in FeValuesList):
+                    FeValuesList.append(oreValue)
+                # print("FeValuesList:",FeValuesList)
+            if("Nickel" in oreTypeStr):
+                oreAbrStr = "Ni"
+                if(oreValue not in NiValuesList):
+                    NiValuesList.append(oreValue)
+            if("Silicon" in oreTypeStr):
+                oreAbrStr = "Si"
+                if(oreValue not in SiValuesList):
+                    SiValuesList.append(oreValue)
+            if("Cobalt" in oreTypeStr):
+                oreAbrStr = "Co"
+                if(oreValue not in CoValuesList):
+                    CoValuesList.append(oreValue)
+            if("Silver" in oreTypeStr):
+                oreAbrStr = "Ag"
+                if(oreValue not in AgValuesList):
+                    AgValuesList.append(oreValue)
+            if("Magnesium" in oreTypeStr):
+                oreAbrStr = "Mg"
+                if(oreValue not in MgValuesList):
+                    MgValuesList.append(oreValue)
+            if("Uraninite" in oreTypeStr):
+                oreAbrStr = "Ur"
+                if(oreValue not in UrValuesList):
+                    UrValuesList.append(oreValue)
+            if("Gold" in oreTypeStr):
+                oreAbrStr = "Au"
+                if(oreValue not in AuValuesList):
+                    AuValuesList.append(oreValue)
+            # print("oreAbrStr:",oreAbrStr)
+            # else:
+            #     pass
+            #     # print("nnn")
+        break
+
+
+def whatOreThatValueIs(valueInt):
+    global FeValuesList
+    global NiValuesList
+    global SiValuesList
+    global CoValuesList
+    global AgValuesList
+    global MgValuesList
+    global UrValuesList
+    global AuValuesList
+    if(valueInt in FeValuesList):
+        return "Fe"
+    if(valueInt in NiValuesList):
+        return "Ni"
+    if(valueInt in SiValuesList):
+        return "Si"
+    if(valueInt in CoValuesList):
+        return "Co"
+    if(valueInt in AgValuesList):
+        return "Ag"
+    if(valueInt in MgValuesList):
+        return "Mg"
+    if(valueInt in UrValuesList):
+        return "Ur"
+    if(valueInt in AuValuesList):
+        return "Au"
+    return "$"
+print("checking xml lists")
+
+
+
 def centeroidnp(arr):
     length = arr.shape[0]
     sum_x = np.sum(arr[:, 0])
@@ -94,7 +197,19 @@ for j in range(128):
             # props.image  # array matching the bbox sub-image
             # print(len(props.coords))  # list of (row,col) pixel indices
             regionSize = len(props.coords)
-            print("regionSize",regionSize)
+            # print("regionSize",regionSize)
+
+            for pt in props.coords:
+                # print("data_ore_layer[pt[0],pt[1]]:",data_ore_layer[pt[0],pt[1]]+128)
+                currentPointValue = data_ore_layer[pt[0], pt[1]] + 0
+                # currentPointValue = data_ore_layer[pt[0],pt[1]]+128
+                # print("currentPointValue:",currentPointValue)
+                ptOreValueIs = whatOreThatValueIs(currentPointValue)
+                # print("ptOreValueIs:",ptOreValueIs)
+                if (ptOreValueIs != "$"):
+                    # print(gpsNameOres.find(ptOreValueIs))
+                    if (gpsNameOres.find(ptOreValueIs) < 0):
+                        gpsNameOres += whatOreThatValueIs(currentPointValue)
 
             pixelWithValues = []
 
@@ -113,7 +228,7 @@ for j in range(128):
                 pixelWithValues.append([[
                     pointsOfCurrentDetectedLackArray[iPoint, 0], pointsOfCurrentDetectedLackArray[iPoint, 1]],[
                                        data_ore_layer[pointsOfCurrentDetectedLackArray[iPoint,0],pointsOfCurrentDetectedLackArray[iPoint,1]]]])
-            print(pixelWithValues)
+            # print(pixelWithValues)
 
             centroid_surface_lack = centeroidnp(pointsOfCurrentDetectedLackArray)
             centroid_surface_lack_array = arr.array('d', [centroid_surface_lack[0], centroid_surface_lack[1]])
@@ -122,10 +237,19 @@ for j in range(128):
             centroid_underground_lack_array = arr.array('d',
                                                         [centroid_underground_lack[0], centroid_underground_lack[1]])
 
-            print("centroid_underground_lack_array",centroid_underground_lack_array)
+            # print("centroid_underground_lack_array",centroid_underground_lack_array)
 
             patternNUmber += 1
-            print("patternNUmber",patternNUmber)
+            # print("patternNUmber",patternNUmber)
+
+            # print("gpsNameOres",gpsNameOres)
+
+            # for c#
+            # // Dynamic ArrayList with no size limit.
+            # List<int> numberList = new List<int>();
+            # numberList.Add(32);
+            # print("List<String> stringList = new List<String>();")
+            print("stringList.add("+"\""+str(centroid_underground_lack_array[0])+","+str(centroid_underground_lack_array[1])+","+gpsNameOres+"\");")
 
 
         pass
