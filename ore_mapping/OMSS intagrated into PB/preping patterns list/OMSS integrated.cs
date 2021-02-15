fd509848b0447b7f7f91791356ee1da8d79a9bc2
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2021-02-15 23:44:53 +0100

    Initializing subpattern infos

--- ore_mapping/OMSS intagrated into PB/preping patterns list/OMSS integrated.cs
+++ ore_mapping/OMSS intagrated into PB/preping patterns list/OMSS integrated.cs
@@ -1,3 +1,7 @@
+List<String> stringList = new List<String>();
+
+List<Vector2D> oreCoords2DSubPattern = new List<Vector2D>();
+
 public Program()
 {
     // The constructor, called only once every session and
@@ -10,6 +14,51 @@ public Program()
     // It's recommended to set RuntimeInfo.UpdateFrequency 
     // here, which will allow your script to run itself without a 
     // timer block.
+
+    stringList.Add("5.285714285714286,50.857142857142854,SiNiMg");
+    stringList.Add("7.266666666666667,17.666666666666668,SiUrMg");
+    stringList.Add("8.666666666666666,88.125,UrMgSi");
+    stringList.Add("10.785714285714286,112.14285714285714,AgAu");
+    stringList.Add("23.434782608695652,49.08695652173913,UrAuFe");
+    stringList.Add("26.785714285714285,76.14285714285714,AgAu");
+    stringList.Add("34.22857142857143,16.685714285714287,MgSiNi");
+    stringList.Add("36.666666666666664,109.125,UrMgSi");
+    stringList.Add("42.833333333333336,42.333333333333336,FeNiCo");
+    stringList.Add("51.22857142857143,70.68571428571428,MgSiNi");
+    stringList.Add("60.56666666666667,7.033333333333333,FeNiCo");
+    stringList.Add("62.88235294117647,104.32352941176471,FeAuUr");
+    stringList.Add("69.88235294117646,29.323529411764707,FeAuUr");
+    stringList.Add("70.78571428571429,55.142857142857146,AgAu");
+    stringList.Add("78.26666666666667,79.66666666666667,SiUrMg");
+    stringList.Add("88.78571428571429,108.14285714285714,AgAu");
+    stringList.Add("90.66666666666667,13.125,UrMgSi");
+    stringList.Add("92.28571428571429,53.857142857142854,SiNiMg");
+    stringList.Add("96.66666666666667,82.125,UrMgSi");
+    stringList.Add("112.83333333333333,18.333333333333332,FeNiCo");
+    stringList.Add("114.70833333333333,83.66666666666667,UrAgCo");
+    stringList.Add("116.41666666666667,52.083333333333336,UrAgCo");
+    stringList.Add("115.26666666666667,112.66666666666667,SiUrMg");
+
+    foreach(var str in stringList)
+    {
+        Echo("str" + str);
+
+        // using the method 
+        String[] strlist = str.Split(',');
+        Echo(strlist[0]);
+        Echo(strlist[1]);
+        Echo(strlist[2]);
+        float tmpx = float.Parse(strlist[0]);
+        float tmpy = float.Parse(strlist[1]);
+        /*int tmpx = 0;
+        int tmpy = 0;*/
+        //string ores = "lol";
+        string ores = strlist[1];
+        
+        oreCoords2DSubPattern.Add(new Vector2D(tmpx, tmpy));
+    }
+    Echo(oreCoords2DSubPattern.Count+"");
+
 }
 
 public void Save()
