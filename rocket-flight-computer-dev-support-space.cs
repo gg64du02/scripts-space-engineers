b78cf5fa4412f3e61b91d319fde3469eb78d9747
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2021-05-15 23:35:01 +0200

    Working on detection if target is onto a stock planet or in space

--- rocket-flight-computer-dev-support-space.cs
+++ rocket-flight-computer-dev-support-space.cs
@@ -623,6 +623,99 @@ public void Main(string argument)
     //Echo("speedRoll:" + speedRoll);
 	
 	
+	//Every stock planets center
+	List<String> listOfPlanetsGPSString = new List<String>();
+	listOfPlanetsGPSString.Add("GPS:EarthLike:0.50:0.50:0.50:");
+	listOfPlanetsGPSString.Add("GPS:Moon:16384.50:136384.50:-113615.50:");
+	listOfPlanetsGPSString.Add("GPS:Mars:1031072.50:131072.50:1631072.50:");
+	listOfPlanetsGPSString.Add("GPS:Europa:916384.50:16384.50:1616384.50:");
+	listOfPlanetsGPSString.Add("GPS:Triton:-284463.50:-2434463.50:365536.50:");
+	listOfPlanetsGPSString.Add("GPS:Pertam:-3967231.50:-32231.50:-767231.50:");
+	listOfPlanetsGPSString.Add("GPS:Alien:131072.50:131072.50:5731072.50:");
+	listOfPlanetsGPSString.Add("GPS:Titan:36384.50:226384.50:5796384.50:");
+	MyWaypointInfo tmpTestPlanetCenter = new MyWaypointInfo("dnm", 0, 0, 0);
+	Vector3D detectedPlanet = new Vector3D(0,0,0);
+	foreach(string str in listOfPlanetsGPSString){
+		tmpTestPlanetCenter = new MyWaypointInfo("dnm", 0, 0, 0);
+		MyWaypointInfo.TryParse(str, out tmpTestPlanetCenter);
+		//Echo("tmpTestPlanetCenter"+tmpTestPlanetCenter);
+		Vector3D tmpVector3DplanetCenter = tmpTestPlanetCenter.Coords;
+		//Echo("tmpVector3DplanetCenter"+tmpVector3DplanetCenter);
+		 Vector3D vector3DToPlanetCenter = tmpVector3DplanetCenter - myPos;
+		 double distanceToPlanetCenter = vector3DToPlanetCenter.Length();
+		//Echo("distanceToPlanetCenter"+distanceToPlanetCenter);
+		if(distanceToPlanetCenter < 100000){
+			detectedPlanet = tmpVector3DplanetCenter;
+			/*
+			customDataStrBuild += "planet's center:\n\n";
+			customDataStrBuild += str;
+			customDataStrBuild += "\n\n\nalign this planet's center with one in the following list:\n";
+			// */
+			break;
+		}
+	}
+	string planetsName = tmpTestPlanetCenter.Name;
+	Echo("planetsName: "+planetsName);
+	// https://www.spaceengineerswiki.com/Planet#Gravitational_Field
+	double minR = 0;
+	double maxR = 0;
+	double nominalGonGround = 0;
+	double atmosphereRadius = 0;
+	//choose the appropriate settings to use for the detected planet
+	//or adapt to local elevation of the firstController?
+	if(planetsName == "Alien"){
+		nominalGonGround = 1;
+		minR = 59400;
+		maxR = 67200;
+		atmosphereRadius = 105000;
+	}
+	if(planetsName == "EarthLike"){
+		nominalGonGround = 1;
+		minR = 59400;
+		maxR = 67200;
+		atmosphereRadius = 105000;
+	}
+	if(planetsName == "Europa"){
+		nominalGonGround = 0.25;
+		minR = 9215;
+		maxR = 10070;
+		atmosphereRadius = 16625;
+	}
+	if(planetsName == "Mars"){
+		nominalGonGround = 1;
+		minR = 59400;
+		maxR = 67200;
+		atmosphereRadius = 105000;
+	}
+	if(planetsName == "Moon"){
+		nominalGonGround = 0.25;
+		minR = 9215;
+		maxR = 9785;
+		atmosphereRadius = 16625;
+	}
+	if(planetsName == "Pertam"){
+		nominalGonGround = 1.2;
+		minR = 29314.8379;
+		maxR = 30818.1621;
+		atmosphereRadius = 52616.375;
+		//addPertamSubPattern();
+	}
+	if(planetsName == "Titan"){
+		nominalGonGround = 0.25;
+		minR = 9215;
+		maxR = 9785;
+		atmosphereRadius = 16625;
+		//addEuropaMoonTitanSubPattern();
+	}
+	if(planetsName == "Triton"){
+		nominalGonGround = 1;
+		minR = 38120.1758;
+		maxR = 48151.8;
+		atmosphereRadius = 70221.375;
+		//addEarthLikeAlienTritonSubPattern();
+	}
+	
+	
 	if (gravityVector.LengthSquared() == 0)
 	{
 		
