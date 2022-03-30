dfe83cb877f69be72762be076c6121a52092484b
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2022-03-30 22:43:40 +0200

    Init

---
+++ rover_autopilot_gg_vanilla/1. GIMP g1.py
@@ -0,0 +1,33 @@
+#
+# import cv2 as cv
+# import numpy as np
+# from matplotlib import pyplot as plt
+# files = {"back.png","front.png"}
+# for file in files:
+#     print("file:",file)
+#     img = cv.imread(file,0)
+#     plt.imshow(img)
+#     # plt.imshow(img, cmap='gray')
+#     plt.show()
+
+# generate gradient maps to know where to avoid and build a stock terrain aware autopilot rover script
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+files = {"back.png"}
+# files = {"front.png","back.png"}
+for file in files:
+    print("file:",file)
+    img = cv.imread(file,0)
+    # plt.imshow(img)
+    # if(bool(img)!=None):
+    #     print("img is empty: exiting")
+    #     exit()
+    sobelx = cv.Sobel(img,cv.CV_64F,1,0,ksize=1)
+    sobely = cv.Sobel(img,cv.CV_64F,0,1,ksize=1)
+    sobelxy = np.add(sobelx , sobely)
+    plt.imshow(sobelxy,cmap='gray')
+    plt.show()
+    # img.close()
+    exit()
+    # img.delete()
