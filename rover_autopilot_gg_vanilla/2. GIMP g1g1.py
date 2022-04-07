4b0c3f1b21dc94492481b0095504a18493f56947
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2022-04-07 21:20:24 +0200

    Experimenting with horizontal lines with openCV

---
+++ rover_autopilot_gg_vanilla/2. GIMP g1g1.py
@@ -0,0 +1,42 @@
+
+# generate gradient maps to know where to avoid and build a stock terrain aware autopilot rover script
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+files = {"back_thres_abs_sobelxy.png"}
+# files = {"front.png","back.png"}
+# files = {"back.png","down.png","front.png","left.png","right.png","up.png"}
+for file in files:
+    print("file:",file)
+
+    img = cv.imread(file,0)
+
+    # debugging purpose
+    # display image before modifying
+    # plt.imshow(img,cmap='gray')
+    ## plt.imshow(img)
+    # plt.show()
+
+    for i in range(0,2048):
+        for j in range(0,2048):
+            if( i % 32==0):
+                if( j % 32==0):
+
+                    # horizontals
+                    print("i",i,"j",j)
+                    uninterruptedLine = True
+                    for pixels in range(0,32):
+                        if(img[i+pixels][j]==128):
+                            uninterruptedLine = False
+                    if(uninterruptedLine == True):
+                        # cv.line(img,(i,j),(i+32,j),(255,255,255))
+                        # bug? for some reason cv.line switch the axis ?
+                        cv.line(img,(j,i),(j,i+32),(255,255,255))
+                    # print()
+
+
+    plt.imshow(img,cmap='gray')
+    ## plt.imshow(img)
+    plt.show()
