0abffb97c2aa68ca6ab4109bf0f3ad899db10e06
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2022-07-18 23:55:17 +0200

    v0.1, able to go toward a given gps into argument

---
+++ rover_autopilot_gg_vanilla/autopilot_TA_v0.1.cs
@@ -0,0 +1,434 @@
+
+//public PID PowerController = new PID(2, 0, .1, 1);
+public string Animation = "=|=";
+public List<IMyMotorSuspension> Wheels = new List<IMyMotorSuspension>();
+public IMyRemoteControl RemoteControl;
+public IMySensorBlock Sensor;
+
+public Vector3D myTerrainTarget = new Vector3D(0,0,0);
+
+MyWaypointInfo myWaypointInfoTerrainTarget = new MyWaypointInfo("target", 0, 0, 0);
+
+IMyRadioAntenna theAntenna = null;
+
+string str_to_display = "";
+		
+public Program()
+{
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed.
+    // 
+    // It's recommended to set RuntimeInfo.UpdateFrequency 
+    // here, which will allow your script to run itself without a 
+    // timer block.
+	
+	var Blocks = new List<IMyTerminalBlock>();
+	GridTerminalSystem.GetBlocks(Blocks);
+	Wheels = Blocks.FindAll(x => x.IsSameConstructAs(Me) && x is IMyMotorSuspension).Select(x => x as IMyMotorSuspension).ToList();
+	RemoteControl = Blocks.Find(x => x.IsSameConstructAs(Me) && x is IMyRemoteControl) as IMyRemoteControl;
+	//Sensor = Blocks.Find(x => x.IsSameConstructAs(Me) && x is IMySensorBlock) as IMySensorBlock;
+
+	
+	theAntenna = Blocks.Find(x => x.IsSameConstructAs(Me) && x is IMyRadioAntenna) as IMyRadioAntenna;
+
+	Runtime.UpdateFrequency = UpdateFrequency.Update10;
+	
+}
+
+public void Save()
+{
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed.
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    // The main entry point of the script, invoked every time
+    // one of the programmable block's Run actions are invoked,
+    // or the script updates itself. The updateSource argument
+    // describes where the update came from.
+    // 
+    // The method itself is required, but the arguments above
+    // can be removed if not needed.
+	
+	if(theAntenna != null){
+		theAntenna.HudText = str_to_display;
+	}
+	
+    //note:
+    //https://github.com/KeenSoftwareHouse/SpaceEngineers/blob/master/Sources/VRage.Math/Vector3D.cs
+    //var targetGpsString = "";
+    //Echo("targetGpsString:" + targetGpsString);
+    MyWaypointInfo myWaypointInfoTarget = new MyWaypointInfo("lol", 0, 0, 0);
+    //MyWaypointInfo.TryParse("GPS:/// #4:53590.85:-26608.05:11979.08:", out myWaypointInfoTarget);
+
+    if (argument != null)
+    {
+        if (argument != "")
+        {
+            Echo("argument:" + argument);
+            if (argument.Contains(":#") == true)
+            {
+                Echo("if (argument.Contains(:#) == true)");
+                MyWaypointInfo.TryParse(argument.Substring(0, argument.Length - 10), out myWaypointInfoTarget);
+                // MyWaypointInfo.TryParse(argument.Substring(0, argument.Length - 10), out myWaypointInfoTerrainTarget);
+            }
+            else
+            {
+                Echo("not if (argument.Contains(:#) == true)");
+                MyWaypointInfo.TryParse(argument, out myWaypointInfoTarget);
+                // MyWaypointInfo.TryParse(argument, out myWaypointInfoTerrainTarget);
+            }
+            if (myWaypointInfoTarget.Coords != new Vector3D(0, 0, 0))
+            {
+                //x,y,z coords is global to remember between each loop
+                myTerrainTarget = myWaypointInfoTarget.Coords;
+				myWaypointInfoTerrainTarget = myWaypointInfoTarget;
+            }
+        }
+    }
+
+    if (myTerrainTarget == new Vector3D(0, 0, 0))
+    {
+        // //using the expected remote control to give us the center of the current planet
+        // flightIndicatorsShipController.TryGetPlanetPosition(out myTerrainTarget);
+    }
+	
+	Echo("myTerrainTarget:"+Vector3D.Round(myTerrainTarget,3));
+	
+	
+	float SLerror = (float) (RemoteControl.SpeedLimit - RemoteControl.GetShipSpeed());
+	
+	if(myTerrainTarget== new Vector3D(0,0,0)){
+		
+		foreach (IMyMotorSuspension Wheel in Wheels)
+		{
+			Wheel.SetValue<Single>("Steer override", 0);
+			Wheel.SetValue<float>("Propulsion override", 0);
+			Wheel.Brake = true;
+			
+			RemoteControl.HandBrake = true;
+			
+			/*
+			float MultiplierPO = (float) Vector3D.Dot(Wheel.WorldMatrix.Up, RemoteControl.WorldMatrix.Right);
+			
+			// str_to_display = ""+"MultiplierPO:"+Math.Round(MultiplierPO,3);
+			// Echo(str_to_display);
+			//SLerror = -0.2f;
+			
+			MyShipVelocities myShipVel = RemoteControl.GetShipVelocities();
+			Vector3D linearSpeedsShip = myShipVel.LinearVelocity;
+			
+			
+			//SLerror =(float) (0 - RemoteControl.GetShipSpeed());
+			SLerror = (float) (-linearSpeedsShip.Dot(RemoteControl.WorldMatrix.Forward));
+			
+			float localPO = -MultiplierPO * SLerror;
+			
+			str_to_display = ""+"localPO:"+Math.Round(localPO,3);
+				
+			if(RemoteControl.GetShipSpeed()<1){
+				
+				Wheel.SetValue<float>("Propulsion override", 0);
+			}
+			else{
+				Wheel.SetValue<float>("Propulsion override", 0.25f*localPO);
+			}
+			*/
+		}
+		
+	}
+	else
+	{
+		int facenumberCalculated = -1;
+		Point pixelPosCalculated = new Point(0,0);
+		
+		faceAndPointOnPlanetsCalculated( RemoteControl,out facenumberCalculated,out pixelPosCalculated,false,new Vector3D(0,0,0));
+		
+		Echo("facenumberMain1:"+facenumberCalculated);
+		Echo("pixelPosMain1:"+pixelPosCalculated);
+
+		whichFileShouldIlook(facenumberCalculated);
+		
+		Vector3D targetV3Dabs = myWaypointInfoTerrainTarget.Coords;
+		
+		Echo("targetV3Dabs:"+Vector3D.Round(targetV3Dabs,3));
+		
+		
+		
+		int facenumberCalculatedTarget = -1;
+		Point pixelPosCalculatedTarget = new Point(0,0);
+		
+		faceAndPointOnPlanetsCalculated( RemoteControl,out facenumberCalculatedTarget,out pixelPosCalculatedTarget,true,targetV3Dabs);
+		
+		
+		Echo("facenumberCalculatedTarget:"+facenumberCalculatedTarget);
+		Echo("pixelPosCalculatedTarget:"+pixelPosCalculatedTarget);
+		
+		whichFileShouldIlook(facenumberCalculatedTarget);
+		
+		bool targetIsOnTheSameFace = false;
+		if(facenumberCalculatedTarget==facenumberCalculated){
+			targetIsOnTheSameFace = true;
+		}
+		else{
+			targetIsOnTheSameFace = false;
+		}
+		Echo("targetIsOnTheSameFace:"+targetIsOnTheSameFace);
+		
+		
+		//getting vectors to help with angles proposals
+		Vector3D shipForwardVector = RemoteControl.WorldMatrix.Forward;
+		Vector3D shipLeftVector = RemoteControl.WorldMatrix.Left;
+		Vector3D shipDownVector = RemoteControl.WorldMatrix.Down;
+		
+		double steerOverride = 0;
+		// double steerOverride = shipForwardVector.Dot(Vector3D.Normalize(targetV3Dabs));
+		// steerOverride*=100;
+		// Echo("steerOverride:"+Math.Round(steerOverride,3));
+		
+		Vector3D targetV3Drel = RemoteControl.GetPosition()-targetV3Dabs;
+		
+		Vector3D crossForwardTT = shipForwardVector.Cross((targetV3Drel));
+		// Vector3D crossForwardTT = shipForwardVector.Cross(Vector3D.Normalize(targetV3Dabs));
+		double turnRightOrLeft = crossForwardTT.Dot(shipDownVector);
+		
+		Echo("turnRightOrLeft:"+Math.Round(turnRightOrLeft,3));
+		
+		// str_to_display = ""+"turnRightOrLeft:"+Math.Round(turnRightOrLeft,3);
+		
+		
+		steerOverride = turnRightOrLeft/crossForwardTT.Length();
+		if(Math.Abs(steerOverride)<.98){
+			steerOverride*=0.25;
+		}
+		// steerOverride*=0.25;
+		
+		steerOverride*=-1;
+		// str_to_display = ""+"steerOverride:"+Math.Round(steerOverride,3);
+		Echo("steerOverride:"+Math.Round(steerOverride,3));
+		
+		
+		steerOverride = MyMath.Clamp(Convert.ToSingle(steerOverride), Convert.ToSingle(-1), Convert.ToSingle(1));
+
+		
+		
+		foreach (IMyMotorSuspension Wheel in Wheels)
+		{
+			double areThisFrontWheel = shipForwardVector.Dot(Wheel.GetPosition() - RemoteControl.GetPosition());
+			Echo("areThisFrontWheel:"+Math.Round(areThisFrontWheel,3));
+			
+			float MultiplierPO = (float) Vector3D.Dot(Wheel.WorldMatrix.Up, RemoteControl.WorldMatrix.Right);
+			
+			// str_to_display = ""+"MultiplierPO:"+Math.Round(MultiplierPO,3);
+			// Echo(str_to_display);
+			//SLerror = -0.2f;
+			
+			float localPO = -MultiplierPO * SLerror;
+			
+			str_to_display = ""+"localPO:"+Math.Round(localPO,3);
+				
+			if(areThisFrontWheel>0){
+				Wheel.SetValue<Single>("Steer override", Convert.ToSingle(steerOverride));
+				Wheel.SetValue<float>("Propulsion override", localPO);
+				
+			}
+			else{
+				// Wheel.SetValue<Single>("Steer override", Convert.ToSingle(-steerOverride));
+				Wheel.SetValue<float>("Propulsion override", localPO);
+			}
+			
+		}
+		
+		//stop when destination is reached
+		if(targetV3Drel.Length()<5){
+			myTerrainTarget = new Vector3D(0, 0, 0);
+		}
+		
+	}
+	
+	
+	// if (!RemoteControl.IsAutoPilotEnabled) {
+	// }
+}
+
+
+
+public void faceAndPointOnPlanetsCalculated(IMyRemoteControl sc,out int facenumber,out Point pixelPos,bool debugMode,Vector3D testedV3D){
+	
+	// Echo(Me.GetPosition()+"");
+	Vector3D myPos = sc.GetPosition();
+	if(debugMode==true){
+		myPos = testedV3D;
+	}
+	
+	// foreach	(Point point in tmpTestNextPoints){
+		// Echo("point"+point);
+	// }
+	
+	Vector3D centerFacePositionOffset = new Vector3D(0,0,0);
+	int planet_radius = 60000;
+	
+	Vector3D planetCenter = new Vector3D(0,0,0);
+
+	bool planetDetected = sc.TryGetPlanetPosition(out planetCenter);
+	
+	Echo("planetCenter:"+planetCenter);
+	
+	// planet_radius = (int) (planetCenter-myPos).Length();
+	planet_radius = (int) (myPos-planetCenter).Length();
+	
+	Echo("planet_radius:"+planet_radius);
+	
+	Vector3D myPosRelToCenter = (myPos-planetCenter);
+	
+	double myPosXAbs = Math.Abs(myPosRelToCenter.X);
+	double myPosYAbs = Math.Abs(myPosRelToCenter.Y);
+	double myPosZAbs = Math.Abs(myPosRelToCenter.Z);
+	
+	Vector3D projectedSphereVector  = new Vector3D(0,0,0);
+	
+	int faceNumber = -1;
+	
+	double pixelScalingToIGW = (2*planet_radius/2048);
+	
+	//shorter names formulas
+	double intX = 0;
+	double intY = 0;
+	double intZ = 0;
+	
+	Point extractedPoint = new Point(0,0);
+	double extractionX_pointRL = 0;
+	double extractionY_pointRL = 0;
+	
+	if(myPosXAbs>myPosYAbs){
+		if(myPosXAbs>myPosZAbs){
+			projectedSphereVector = (planet_radius/myPosXAbs)*myPosRelToCenter;
+			intY = projectedSphereVector.Y;
+			intZ = projectedSphereVector.Z;
+			if(myPosRelToCenter.X>0){
+				faceNumber = 3;
+				extractionX_pointRL = planet_radius - intY;
+				extractionY_pointRL = planet_radius - intZ;
+			}
+			else{
+				faceNumber = 4;
+				extractionX_pointRL = planet_radius - intY;
+				extractionY_pointRL = planet_radius + intZ;
+			}
+		}
+	}
+	
+	if(myPosYAbs>myPosXAbs){
+		if(myPosYAbs>myPosZAbs){
+			projectedSphereVector = (planet_radius/myPosYAbs)*myPosRelToCenter;
+			intX = projectedSphereVector.X;
+			intZ = projectedSphereVector.Z;
+			if(myPosRelToCenter.Y>0){
+				faceNumber = 5;
+				extractionY_pointRL = planet_radius - intX;
+				extractionX_pointRL = planet_radius - intZ;
+			}
+			else{
+				faceNumber = 1;
+				extractionY_pointRL = planet_radius + intX ;
+				extractionX_pointRL = planet_radius - intZ ;
+			}
+		}
+	}
+	
+	if(myPosZAbs>myPosXAbs){
+		if(myPosZAbs>myPosYAbs){
+			projectedSphereVector = (planet_radius/myPosZAbs)*myPosRelToCenter;
+			intX = projectedSphereVector.X;
+			intY = projectedSphereVector.Y;
+			if(myPosRelToCenter.Z>0){
+				faceNumber = 0;
+				extractionY_pointRL = planet_radius + intX;
+				extractionX_pointRL = planet_radius - intY;
+			}
+			else{
+				faceNumber = 2;
+				extractionY_pointRL = planet_radius - intX;
+				extractionX_pointRL =  planet_radius - intY;
+			}
+		}
+	}
+	
+	if(extractionX_pointRL==0){
+		//out-ing
+		facenumber =faceNumber;
+		pixelPos = new Point(0,0);
+		
+		return;}
+	
+	if(extractionY_pointRL==0){
+		
+		//out-ing
+		facenumber =faceNumber;
+		pixelPos = new Point(0,0);
+		return;}
+	
+	double tmpCalcX = extractionX_pointRL / pixelScalingToIGW;
+	double tmpCalcY = extractionY_pointRL / pixelScalingToIGW;
+	
+	extractedPoint = new Point((int)tmpCalcX,(int)tmpCalcY);
+	
+	//Echo("extractedPoint:"+extractedPoint);
+	//Echo("faceNumber:"+faceNumber);
+	//Echo("projectedSphereVector:"+projectedSphereVector);
+	
+	Point calculatedPoint = new Point(-1,-1);
+	
+	
+	//out-ing
+	facenumber =faceNumber;
+	pixelPos =extractedPoint;
+	
+}
+
+public void whichFileShouldIlook(int facenumber){
+	
+	string tmpStr = ""+facenumber+" is ";
+	
+	if(facenumber==0){
+		tmpStr += "back";
+	}
+	if(facenumber==1){
+		tmpStr += "down";
+	}
+	
+	if(facenumber==2){
+		tmpStr += "front";
+	}
+	if(facenumber==3){
+		tmpStr += "left";
+	}
+	
+	if(facenumber==4){
+		tmpStr += "right";
+	}
+	if(facenumber==5){
+		tmpStr += "up";
+	}
+	
+	Echo(tmpStr);
+	
+	// 0 is back
+	// 1 is down
+
+	// 2 is front
+	// 3 is left
+
+	// 4 is right
+	// 5 is up
+}
+
