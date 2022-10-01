c6ebfa27db286a6fc7ba1a48ac1896abadae427e
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2022-10-01 17:20:52 +0200

    WIP generating the voronoi diagram

--- rover_autopilot_gg_vanilla/voronoi_with_kdtree.py
+++ rover_autopilot_gg_vanilla/voronoi_with_kdtree.py
@@ -11,8 +11,8 @@ folderNameSource = "C:/github_ws/scripts-space-engineers/rover_autopilot_gg_vani
 
 
 # files = {"back.png","down.png","front.png","left.png","right.png","up.png"}
-# files = {"back_sobelBitmap_64_min.pickle"}
-files = {"back_sobelBitmap_64_min.pickle","down_sobelBitmap_64_min.pickle","front_sobelBitmap_64_min.pickle","left_sobelBitmap_64_min.pickle","right_sobelBitmap_64_min.pickle","up_sobelBitmap_64_min.pickle"}
+files = {"back_sobelBitmap_64_min.pickle"}
+# files = {"back_sobelBitmap_64_min.pickle","down_sobelBitmap_64_min.pickle","front_sobelBitmap_64_min.pickle","left_sobelBitmap_64_min.pickle","right_sobelBitmap_64_min.pickle","up_sobelBitmap_64_min.pickle"}
 
 # sobelBitmap_64_min_TargetFilePath = stringTmpSplitted + "_sobelBitmap_64_min.pickle"
 
@@ -123,9 +123,11 @@ for file_path in full_files_path:
                 # debug
                 # X.append([0,x,y])
 
-                # TODO:faire la transformation en sphere ?
                 pointIn3D = conv2dTo3D([x,y], intFaceNumber)
                 # print("pointIn3D",pointIn3D)
+                # TODO:faire la transformation en sphere ?
+                l2norm = np.linalg.norm(pointIn3D,2)
+                pointIn3D = pointIn3D / l2norm
                 X.append(pointIn3D)
 
                 pass
@@ -133,45 +135,64 @@ for file_path in full_files_path:
 
 # appending to the planets points
 # X is going to be the planets points
-pass
 print("number of points",len(X))
 tree = KDTree(X, leaf_size=2)
-
 # dist, ind = tree.query(X[:1], k=3)
 dist, ind = tree.query([[500,500,PR]], k=5)
 print(ind)  # indices of 5 closest neighbors
-
-print(dist)  # distances to  closest neighbors
-
-
-
-# data = np.load("LIDAR_0.npy")
-# # data.dtype is [('x', '<f4'), ('y', '<f4'), ('z', '<f4'), ('intensity', '<f4'), ('t_low', '<u4'), ('t_high', '<u4')]
+print(dist)  # distances to 5 closest neighbors
 
 xs = []
 ys = []
 zs = []
-
 for t3dPoint in range(len(X)):
     xs.append(X[t3dPoint][0])
     ys.append(X[t3dPoint][1])
     zs.append(X[t3dPoint][2])
 
-# # coordinates
-# xs = X[:,0]
-# ys = X[:,1]
-# zs = X[:,2]
-# # attribute
-# t_low = data["t_low"]
-
 import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(12,7))
 ax = fig.add_subplot(projection='3d')
-# img = ax.scatter(xs, ys, zs, c=t_low, cmap=plt.hot())
-img = ax.scatter(xs, ys, zs)
+# img = ax.scatter(xs, ys, zs,s=1)
+img = ax.scatter(xs, ys, zs,s=0.2)
 fig.colorbar(img)
 
 plt.show()
 pass
 
+
+VoronoiAccumulatorSphere = VAS = []
+
+# getting the radius without obstacles for each point
+for file_path in full_files_path:
+
+    with open(file_path,'rb') as f:
+        img = pickle.load(f)
+
+    stringTmpSplitted = file_path.split("/")[-1:][0].split('_')[0]
+    # .split('_')[0]
+    print("stringTmpSplitted:",stringTmpSplitted)
+
+    intFaceNumber = numberOfStrFace(stringTmpSplitted)
+    print("intFaceNumber:",intFaceNumber)
+
+    # converting 2d to 3d point:
+    for x in range(0,2048):
+        for y in range(0,2048):
+            pointIn3D = conv2dTo3D([x,y], intFaceNumber)
+            l2norm = np.linalg.norm(pointIn3D,2)
+            pointIn3D = pointIn3D / l2norm
+            if(img[x,y]==0):
+
+                dist, ind = tree.query([pointIn3D], k=1)
+
+                # print(ind)  # indices of 1 closest neighbors
+                print(dist)  # distances to 1 closest neighbors
+
+                #TODO: labelled obstacles in 3d
+                # VAS.append()
+                pass
+            else:
+                pass
+                #TODO: VAS.append([pointIn3D,label?])
