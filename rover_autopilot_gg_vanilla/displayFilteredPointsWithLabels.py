4efe68ce7bbd58f64bc99641a288cebbe34d4b98
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2022-10-16 16:08:40 +0200

    Renaming

--- rover_autopilot_gg_vanilla/displayFilteredPointsWithLabels.py
+++ rover_autopilot_gg_vanilla/displayFilteredPointsWithLabels.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import KDTree
 
 folderNameSource = "C:/github_ws/scripts-space-engineers/rover_autopilot_gg_vanilla/game_data/SS/PlanetDataFiles/Pertam/"
 
-file_path = folderNameSource + "dVASpertamFiltered.pickle"
+file_path = folderNameSource + "dVASpertamFilteredWithLabels.pickle"
 
 with open(file_path,'rb') as f:
     dVASfiltered = pickle.load(f)
@@ -18,6 +18,8 @@ with open(file_path,'rb') as f:
 xs = []
 ys = []
 zs = []
+
+mucolors = []
 
 for key in dVASfiltered.keys():
     pass
@@ -28,6 +30,8 @@ for key in dVASfiltered.keys():
     xs.append(key[0])
     ys.append(key[1])
     zs.append(key[2])
+    mucolors.append(key[2]%3000)
+    # mucolors.append((key[0]+key[1]+key[2])%100)
 
 # dist, ind = tree.query([[500,500,PR]], k=5)
 # print(ind)  # indices of 5 closest neighbors
@@ -43,8 +47,28 @@ import matplotlib.pyplot as plt
 fig = plt.figure(figsize=(12,7))
 ax = fig.add_subplot(projection='3d')
 # img = ax.scatter(xs, ys, zs,s=1)
-img = ax.scatter(xs, ys, zs,s=0.2)
+# img = ax.scatter(xs, ys, zs,s=0.2)
+img = ax.scatter(xs, ys, zs, c=mucolors,s=0.2)
+# img = ax.scatter(xs, ys, s=0.2)
 fig.colorbar(img)
 
 plt.show()
 pass
+
+counting2 = 0
+counting3 = 0
+counting4 = 0
+for key in dVASfiltered.keys():
+    pass
+    # print(key, '->', dVASfiltered[key])
+    counted = dVASfiltered[key]
+    if(counted==2):
+        counting2 = counting2 + 1
+    if(counted==3):
+        counting3 = counting3 + 1
+    if(counted==4):
+        counting4 = counting4 + 1
+
+print("counting2",counting2)
+print("counting3",counting3)
+print("counting4",counting4)
