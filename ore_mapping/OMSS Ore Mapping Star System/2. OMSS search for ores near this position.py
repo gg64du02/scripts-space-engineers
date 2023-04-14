ca2c3f99fe83616850291ab72f8e3f22674efe3c
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2023-04-14 23:43:13 +0200

    Doing a request on generated data

--- ore_mapping/OMSS Ore Mapping Star System/2. OMSS search for ores near this position.py
+++ ore_mapping/OMSS Ore Mapping Star System/2. OMSS search for ores near this position.py
@@ -54,7 +54,13 @@ def parseGPS(stringGps):
 # Moon
 # myCurrentPosArray =  np.asarray([13006.69678417,145040.712265219,-115439.960197295])
 # MyPos
-myCurrentPosArray,_ =  parseGPS("GPS:#31:-36112.76:6750.47:49163.05:#FF75C9F1:")
+# myCurrentPosArray,_ =  parseGPS("GPS:#31:-36112.76:6750.47:49163.05:#FF75C9F1:")
+# myCurrentPosArray,_ =  parseGPS("GPS:/// #1:-2389.6:24834.15:16766.67:#FF75C9F1:")
+myCurrentPosArray,_ =  parseGPS("GPS:we buy ores itdr:-4802.35:29730.55:-2251.3:#FF75C9F1:")
+# myCurrentPosArray,_ =  parseGPS("GPS:/// #29:-27015.78:13238.14:1261.6:#FF75C9F1:")
+
+
+
 
 # Every stock planets center
 listOfPlanetsGPSString = []
@@ -96,7 +102,9 @@ listOfOresOnThePlanet = []
 
 # print(os.path.join(folder_planetsfiles,planetCenterString,'Triton_ores.png'))
 # filename = os.path.join(folder_planetsfiles,planetCenterString,'Triton_ores.txt')
-filename = os.path.join(folder_planetsfiles,planetCenterString,planetCenterString+'_ores.txt')
+# filename = os.path.join(folder_planetsfiles,planetCenterString,planetCenterString+'_ores.txt')
+filename = os.path.join(folder_planetsfiles,planetCenterString,planetCenterString+'_ores2.txt')
+# filename = os.path.join(folder_planetsfiles,planetCenterString,planetCenterString+'_up_under_21.txt')
 print("filename",filename)
 
 # f = open(filename, "r")
@@ -118,7 +126,7 @@ for x in f:
 f.close()
 
 
-rangeToCheckAt = 2000
+rangeToCheckAt = 800
 # range to check ORES
 listOfOresOnThePlanetNearMyPos = []
 for GPSString in listOfOresOnThePlanet:
