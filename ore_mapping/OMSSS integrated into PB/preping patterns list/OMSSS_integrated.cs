a30c14002aaf39577b03529605b70e1852e8012b
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2024-07-27 19:53:18 +0200

    Debug line disabled

--- ore_mapping/OMSSS integrated into PB/preping patterns list/OMSSS_integrated.cs
+++ ore_mapping/OMSSS integrated into PB/preping patterns list/OMSSS_integrated.cs
@@ -1096,7 +1096,8 @@ public void Main(string argument, UpdateType updateSource)
 								Echo(intTmp+"--"+intActivatedZero+"-"+intActivatedOne+"-"+intActivatedTwo+"-"+intActivatedThree+"-"+intActivatedFour+"-"+intActivatedFive);
 								Echo("IC:" + Runtime.CurrentInstructionCount);
 								*/
-								Echo(intTmp+"--"+intActivatedZero+"-"+intActivatedOne+"-"+intActivatedTwo+"-"+intActivatedThree+"-"+intActivatedFour+"-"+intActivatedFive+"IC:" + Runtime.CurrentInstructionCount);
+								//Echo(intTmp+"--"+intActivatedZero+"-"+intActivatedOne+"-"+intActivatedTwo+"-"+intActivatedThree+"-"+intActivatedFour+"-"+intActivatedFive+"IC:" + Runtime.CurrentInstructionCount);
+								
 								
 								//if(intTmp==0){
 									intX = 1*(- planet_radius+centroid_surface_lack_planetSized[1]*1);
